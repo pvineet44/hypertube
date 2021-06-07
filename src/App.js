c0d67678ac9718b452cf7b640c0c74a55f3b1b02
Vineet Parekh <pvineet44@gmail.com>
2021-06-07 13:54:29 +0530

    Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      Hypertube project
+    <div className='App'>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path='/'>
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Header() {
+
+    const handleAuth = () => {
+        console.log('handle auth')
+    }
+
+  return (
+    <Nav>
+      <Logo>
+        <img src='/images/logo.svg' alt='Disney+' />
+      </Logo>
+      <Login onClick={handleAuth}>Login / Signup</Login>
+    </Nav>
+  );
+}
+
+const Nav = styled.nav`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 70px;
+  background-color: #090b13;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 36px;
+  letter-spacing: 16px;
+  z-index: 3;
+`;
+
+const Logo = styled.a`
+  padding: 0;
+  width: 80px;
+  margin-top: 4px;
+  max-height: 70px;
+  font-size: 0;
+  display: inline-block;
+  img {
+    display: block;
+    width: 100%;
+  }
+  body {
+    color: #f9f9f9;
+  }
+`;
+
+const Login = styled.a`
+  background-color: rgba(0, 0, 0, 0.6);
+  padding: 8px 16px;
+  text-transform: uppercase;
+  letter-spacing: 1.5px;
+  border: 1px solid #f9f9f9;
+  border-radius: 4px;
+  transition: all 0.2s ease 0s;
+
+  &:hover {
+    background-color: #f9f9f9;
+    color: #000;
+    border-color: transparent;
+  }
+`;
+
+export default Header;
